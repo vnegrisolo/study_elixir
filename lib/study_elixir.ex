20a0e733b77a4411b26fa31c01b07b40c805bb59
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2017-03-31 23:37:29 +0100

    Create elixir project

---
+++ lib/study_elixir.ex
@@ -0,0 +1,18 @@
+defmodule StudyElixir do
+  @moduledoc """
+  Documentation for StudyElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StudyElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
