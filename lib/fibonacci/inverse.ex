9f87a79f237ddd9eeedff58cb5d70aecb40025e4
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2017-04-02 00:16:45 +0100

    Improve readability of fibonacci inverse alg

--- lib/fibonacci/inverse.ex
+++ lib/fibonacci/inverse.ex
@@ -2,8 +2,8 @@ defmodule Fibonacci.Inverse do
   @behaviour Fibonacci
 
   def calc(n) when n < 0, do: raise ArgumentError, message: "negative is invalid"
-  def calc(n), do: calc(n, 1, 1)
+  def calc(n), do: calc(n, 1, 0)
 
   defp calc(0, acc, _), do: acc
-  defp calc(n, acc, next), do: calc(n-1, next, acc + next)
+  defp calc(n, acc, last), do: calc(n-1, acc + last, acc)
 end
