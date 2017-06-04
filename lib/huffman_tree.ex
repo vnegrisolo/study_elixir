023961ebfe4be89e49c061bbf53108bbe82c37da
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2017-06-04 19:45:01 +0100

    Decode huffman tree into string

--- lib/huffman_tree.ex
+++ lib/huffman_tree.ex
@@ -16,30 +16,38 @@ defmodule HuffmanTree do
   def encode(tree, text) do
     text
     |> String.graphemes
-    |> Enum.map(&find_encode(tree, &1))
+    |> Enum.map(&encode_char(&1, tree))
     |> Enum.join
   end
 
-  defp count_chars([], map), do: map
-  defp count_chars([c|rest], map) do
-    increment = fn value -> {value, (value || 0) + 1} end
-    # increment = &{&1, (&1 || 0) + 1}
-    {_value, map} = Map.get_and_update(map, c, increment)
+  def decode(tree, binary), do: find_decode(tree, binary)
+
+  defp count_chars([], map) do
+    map
+  end
+  defp count_chars([char|rest], map) do
+    increment = &{&1, (&1 || 0) + 1}
+    {_value, map} = Map.get_and_update(map, char, increment)
     count_chars(rest, map)
   end
 
   defp sort_chars(map) do
-    map
-    |> Map.keys
-    |> Enum.sort_by(&Map.get(map, &1), &>=/2)
+    map |> Map.keys |> Enum.sort_by(&Map.get(map, &1), &>=/2)
   end
 
   defp to_tree([]), do: nil
-  defp to_tree([c|rest]), do: %Node{left: to_tree(rest), right: c}
+  defp to_tree([char|rest]), do: %Node{left: to_tree(rest), right: char}
 
-  defp find_encode(tree, char, acc \\ "")
-  defp find_encode(%Node{right: c}, c, acc), do: acc <> "1"
-  defp find_encode(tree, c, acc) do
-    find_encode(tree.left, c, acc <> "0")
+  defp encode_char(char, tree, acc \\ "")
+  defp encode_char(char, %Node{right: char}, acc), do: acc <> "1"
+  defp encode_char(char, tree, acc), do: encode_char(char, tree.left, acc <> "0")
+
+  defp find_decode(tree, node \\ nil, binary, acc \\ "")
+  defp find_decode(_tree, _node, "", acc), do: acc
+  defp find_decode(tree, node, "1" <> rest, acc) do
+    find_decode(tree, tree, rest, acc <> (node || tree).right)
+  end
+  defp find_decode(tree, node, "0" <> rest, acc) do
+    find_decode(tree, (node || tree).left, rest, acc)
   end
 end
--- test/lib/huffman_tree_test.exs
+++ test/lib/huffman_tree_test.exs
@@ -1,34 +1,57 @@
 defmodule HuffmanTreeTest do
   use ExUnit.Case, async: true
+  ExUnit.Case.register_attribute __ENV__, :text
 
   alias HuffmanTree.Node
+  import HuffmanTree, only: [create_tree: 1, encode: 2, decode: 2]
 
-  describe "HuffmanTree.create_tree/1" do
-    test "create_tree from string" do
-      assert HuffmanTree.create_tree("ABACCA") == %Node{
-        right: "A",
-        left: %Node{
-          right: "C",
-          left: %Node{
-            right: "B"
-          }
-        }
+  @tree %Node{
+    right: "A",
+    left: %Node{
+      right: "C",
+      left: %Node{
+        right: "B",
+        left: nil
       }
+    }
+  }
+
+  describe "create_tree/1" do
+    test "create_tree from string" do
+      assert create_tree("ABACCA") == @tree
     end
   end
 
-  describe "HuffmanTree.encode/2" do
+  describe "encode/2" do
     test "encode string" do
-      tree = %Node{
-        right: "A",
-        left: %Node{
-          right: "C",
-          left: %Node{
-            right: "B"
-          }
-        }
-      }
-      assert HuffmanTree.encode(tree, "ABACCA") == "1001101011"
+      assert encode(@tree, "ABACCA") == "1001101011"
     end
+  end
+
+  describe "decode/2" do
+    test "decode string" do
+      assert decode(@tree, "1001101011") == "ABACCA"
+    end
+  end
+
+  describe "encode/2 |> decode/2" do
+    ~w[
+      ABACCA
+      ABABABA
+      CBCBCBCC
+      ewfrsdajkhasdfjhasfa
+      eivŃaïve
+    ]
+    |> Enum.with_index
+    |> Enum.each(fn(elem) ->
+      {text, index} = elem
+      @text text
+      test "tree/encode/decode for '#{index}'", context do
+        text = context.registered.text
+        tree = create_tree(text)
+        encoded = encode(tree, text)
+        assert decode(tree, encoded) == text
+      end
+    end)
   end
 end
